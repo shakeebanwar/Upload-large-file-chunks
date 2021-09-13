1e2502bff68835eb5d4a2f90ab06a2d340a90fd5
muhammadharis1 <mh7365496@gmail.com>
2021-09-13 15:34:12 +0500

    done

--- fileUploader/settings.py
+++ fileUploader/settings.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'uploader',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -86,3 +87,17 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+#S3 BUCKETS CONFIG
+
+AWS_ACCESS_KEY_ID = 'AKIA57IYNILPJ45CH7JK'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'shakeeb-crm'
+
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_REGION_NAME = 'us-east-2'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
--- uploader/views.py
+++ uploader/views.py
@@ -2,9 +2,54 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import os
 from .models import File
+from django.conf import settings
+import boto3
+
+AWS_S3_CREDS = {
+        "aws_access_key_id":settings.AWS_ACCESS_KEY_ID,
+        "aws_secret_access_key":settings.AWS_SECRET_ACCESS_KEY
+}
+s3 = boto3.client('s3',**AWS_S3_CREDS)
+
+
+
+def upload_file(file_name, bucket, object_name=None):
+
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+
+def check(request):
+    AWS_S3_CREDS = {
+        "aws_access_key_id":settings.AWS_ACCESS_KEY_ID,
+        "aws_secret_access_key":settings.AWS_SECRET_ACCESS_KEY
+    }
+    s3 = boto3.client('s3',**AWS_S3_CREDS)
+  
+
+    with open("media/hye.txt", "rb") as f:
+        s3.upload_fileobj(f, settings.AWS_STORAGE_BUCKET_NAME, 'hye.txt')
+    
+    return JsonResponse({'data':'Invalid Request'})
+
+
 
 def index(request):
-    if request.method == 'POST':  
+    if request.method == 'POST':
+
+
         file = request.FILES['file'].read()
         fileName= request.POST['filename']
         existingPath = request.POST['existingPath']
@@ -19,6 +64,10 @@ def index(request):
                 path = 'media/' + fileName
                 with open(path, 'wb+') as destination: 
                     destination.write(file)
+                    
+
+
+
                 FileFolder = File()
                 FileFolder.existingPath = fileName
                 FileFolder.eof = end
@@ -51,3 +100,14 @@ def index(request):
                     res = JsonResponse({'data':'No such file exists in the existingPath'})
                     return res
     return render(request, 'upload.html')
+
+
+
+
+
+
+
+
+    
+
+
